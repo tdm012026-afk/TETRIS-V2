8f13cd7f3484056c969766c9ba17e863e6420894
tdm012026-afk <tdm012026@gmail.com>
2026-08-25 09:58:40 +0200

    Draw game bord

--- Tetris-V2/GameForm.cs
+++ Tetris-V2/GameForm.cs
@@ -4,12 +4,42 @@ namespace Tetris_V2
     {
         private const int GridWidth = 10;
         private const int GridHeight = 20;
-        private const int Cellsize = 30;
+        private const int CellSize = 30;
 
         private int[,] grid = new int[GridWidth, GridHeight];
         public GameForm()
         {
             InitializeComponent();
+
+            ClientSize = new Size(
+                GridWidth * CellSize,
+                GridHeight * CellSize
+                );
         }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            for(int x = 0; x < GridWidth; x++)
+            {
+                for (int y = 0; y < GridHeight; y++)
+                {
+                    Rectangle rect = new Rectangle(
+                        x * CellSize,
+                        y * CellSize,
+                        CellSize,
+                        CellSize);
+
+                    g.FillRectangle(Brushes.Black, rect);
+                    g.DrawRectangle(Pens.White, rect);
+                    
+                }
+            }
+        }
+        
+
     }
 }
